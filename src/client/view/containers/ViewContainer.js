c3e1550c26d7dae7b618a53b04d74758d9baae83
jlc488 <jlc488@gmail.com>
2018-12-11 23:12:31 +0800

    update ViewContainer

--- src/client/view/containers/ViewContainer.js
+++ src/client/view/containers/ViewContainer.js
@@ -1,7 +1,25 @@
 import React from "react"
 import PropTypes from "prop-types"
+import { Container, Row, Col } from 'reactstrap'
+import { NavComponent } from "../components"
 
-const ViewContainer = ({ content }) => <div>{content}</div>
+const ViewContainer = ({ content }) => {
+  return (
+  <Container>
+    <Row><Col>&nbsp;</Col></Row>
+    <Row>
+      <Col>
+        <NavComponent />
+      </Col>
+    </Row>
+    <Row>
+      <Col>
+        {content}
+      </Col>
+    </Row>
+  </Container>
+  )
+}
 
 ViewContainer.propTypes = {
   content: PropTypes.element.isRequired
