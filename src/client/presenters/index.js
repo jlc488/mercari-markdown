6b7dad6a8ec28053c674ff9d71a97d53f761776c
jlc488 <jlc488@gmail.com>
2018-12-11 23:11:55 +0800

    adding presenter stubs

---
+++ src/client/presenters/index.js
@@ -0,0 +1,6 @@
+import EditPresenter from './EditPressenter'
+import ListPresenter from "./ListPresenter"
+import ReadPresenter from './ReadPresenter'
+import WritePresenter from './WritePresenter'
+
+export { EditPresenter, ListPresenter, ReadPresenter, WritePresenter}
