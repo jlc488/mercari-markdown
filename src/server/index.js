f83b2931ca540dbf6edb7762bb817a651b243927
jlc488 <jlc488@gmail.com>
2018-12-13 02:37:01 +0800

    add CORS

--- src/server/index.js
+++ src/server/index.js
@@ -12,6 +12,13 @@ const app = express()
 
 app.use(helmet())
 
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept")
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+  next()
+})
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({
   extended: true
