bf411c286f32612144c638c2a5e03f4e9f267259
jlc488 <jlc488@gmail.com>
2018-12-12 17:11:35 +0800

    remove unnecessary files

--- src/client/presenters/index.js
+++
@@ -1,6 +0,0 @@
-import EditPresenter from './EditPressenter'
-import ListPresenter from "./ListPresenter"
-import ReadPresenter from './ReadPresenter'
-import WritePresenter from './WritePresenter'
-
-export { EditPresenter, ListPresenter, ReadPresenter, WritePresenter}
