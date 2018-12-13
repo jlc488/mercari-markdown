5f46c0d5159a4a37e801d2829cf97667cc16f547
sin kang <sin.kang@accedo.tv>
2018-12-13 14:59:02 +0800

    add BaseComponent

---
+++ src/client/view/components/BaseComponent.js
@@ -0,0 +1,30 @@
+/* eslint-disable react/no-unused-state */
+
+import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
+
+class BaseComponent extends Component {
+  static getDerivedStateFromProps() {
+    return { redirectTo: null };
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      redirectTo: null
+    };
+  }
+
+
+  redirectTo(path) {
+    this.setState({ redirectTo: path });
+  }
+
+  renderRedirect() {
+    return this.state.redirectTo
+      ? <Redirect to={this.state.redirectTo} />
+      : null;
+  }
+}
+
+export default BaseComponent;
